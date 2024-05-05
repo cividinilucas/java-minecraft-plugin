f2878133c5ea5dbb13f5b23796ddc0884a869712
Lucas Cividini <lucaspagnussat5@gmail.com>
2024-05-05 16:06:53 -0300

    first real commit

---
+++ src/main/java/com/lucas/plugintest/BlockCommand.java
@@ -0,0 +1,24 @@
+package com.lucas.plugintest;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class BlockCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        if (commandSender instanceof Player){
+            //forçando ser um player a executar o comando
+            Player player = (Player) commandSender;
+            player.sendMessage("Voce kickou");
+
+        }
+
+
+        return false;
+    }
+}
---
+++ src/main/java/com/lucas/plugintest/PluginTest.java
@@ -0,0 +1,21 @@
+package com.lucas.plugintest;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginTest extends JavaPlugin{
+//todas as classes que utilizarao eventos devem implementar o listener
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Plugin enabled");
+
+    getCommand("block").setExecutor(new BlockCommand());
+
+    }
+}
