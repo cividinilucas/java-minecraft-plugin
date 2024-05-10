7994b69ad60f59db7a3eb0a8dbf3d43219c3a5b8
Lucas Cividini <lucaspagnussat5@gmail.com>
2024-05-10 12:51:33 -0300

    changed all the ban logic, using a new specify event to block the player to log in

--- src/main/java/com/lucas/plugintest/UnmuteBlockCommand.java
+++ src/main/java/com/lucas/plugintest/UnmuteBlockCommand.java
@@ -4,8 +4,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class UnmuteBlockCommand implements CommandExecutor {
 
+public class UnmuteBlockCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
 
